49932dc2fd434bab1565ad3f45bf63371b04a3a4
Andrey <cattalin.htc@gmail.com>
2019-08-18 17:42:08 +0200

    crud songs

--- app/v1.0/routes.js
+++ app/v1.0/routes.js
@@ -12,6 +12,7 @@ api.get('/test', (req, res) => {
 api.post('/songs', song.createSong)
 api.get('/songs', song.getAllSongs)
 api.get('/songs/:id', song.getSongById)
+api.patch('/songs/:id', song.updateSong)
 api.delete('/songs/:id', song.deleteSong)
 
 module.exports = {
